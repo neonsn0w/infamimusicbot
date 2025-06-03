d7c9b43ed2357afd86d1b0ed4cb4a7c676c6f79d
neonsn0w <pietro@eprase.com>
2025-06-03 09:21:42 +0200

    Made the konami command ephemeral

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ async def on_ready():
 
 @bot.tree.command(name="konami", description="You forgot the Konami Code, didnt you?")
 async def konami(interaction: discord.Interaction):
-    await interaction.response.send_message("Konami Code: :arrow_up: :arrow_up: :arrow_down: :arrow_down: :arrow_left: :arrow_right: :arrow_left: :arrow_right: :regional_indicator_b: :regional_indicator_a:")
-
+    await interaction.response.send_message("Konami Code: :arrow_up: :arrow_up: :arrow_down: :arrow_down: :arrow_left: :arrow_right: :arrow_left: :arrow_right: :regional_indicator_b: :regional_indicator_a:", ephemeral=True)
 
 bot.run(TOKEN)
